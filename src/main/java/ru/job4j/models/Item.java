4007b2c29d8f7f81748b84c57b1a38f28a59e49f
eRqa <ivlasov.spb@gmail.com>
2021-05-17 17:16:44 +0300

    Initial commit

---
+++ src/main/java/ru/job4j/models/Item.java
@@ -0,0 +1,77 @@
+package ru.job4j.models;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "items")
+public class Item {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "created")
+    private Date created;
+
+    @Column(name = "done")
+    private boolean done;
+
+    public Item() {
+    }
+
+    public Item(int id, String description, Date created, boolean done) {
+        this.id = id;
+        this.description = description;
+        this.created = created;
+        this.done = done;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public boolean isDone() {
+        return done;
+    }
+
+    public void setDone(boolean done) {
+        this.done = done;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", description='" + description + '\'' +
+                ", created=" + created +
+                ", done=" + done +
+                '}';
+    }
+
+
+}
