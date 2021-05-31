1889b4fdbbbb954c3e70404066642db565fb1ca1
eRqa <ivlasov.spb@gmail.com>
2021-05-31 21:29:24 +0300

    Рефакторинг. Применение шаблона проектирования wrapper

--- src/main/java/ru/job4j/HbmToDoList.java
+++ src/main/java/ru/job4j/HbmToDoList.java
@@ -2,12 +2,14 @@ package ru.job4j;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 import org.hibernate.boot.MetadataSources;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import ru.job4j.models.Item;
 
 import java.util.List;
+import java.util.function.Function;
 
 public class HbmToDoList implements Store {
 
@@ -28,53 +30,59 @@ public class HbmToDoList implements Store {
 
     @Override
     public Item add(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        session.save(item);
-        session.getTransaction().commit();
-        session.close();
-        return item;
+        return tx(
+                session -> {
+                    session.save(item);
+                    return item;
+                }
+        );
     }
 
     @Override
     public boolean revertDone(Item item) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        item.setDone(!item.isDone());
-        session.update(item);
-        session.getTransaction().commit();
-        session.close();
-        return item.isDone();
+        return tx(
+                session -> {
+                    item.setDone(!item.isDone());
+                    session.update(item);
+                    return item.isDone();
+                }
+        );
     }
 
     @Override
     public List<Item> getAllTasks() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> result = session.createQuery("from Item").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return tx(
+                session -> session.createQuery("from Item").list()
+        );
     }
 
     @Override
     public List<Item> getCompleted() {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        List<Item> result = session.createQuery("from Item WHERE done = true").list();
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return tx(
+                session -> session.createQuery("from Item WHERE done = true").list()
+        );
     }
 
     @Override
     public Item findItemById(int id) {
-        Session session = sf.openSession();
-        session.beginTransaction();
-        Item result = session.get(Item.class, id);
-        session.getTransaction().commit();
-        session.close();
-        return result;
+        return tx(
+                session -> session.get(Item.class, id)
+        );
+    }
+
+    private <T> T tx(final Function<Session, T> command) {
+        final Session session = sf.openSession();
+        final Transaction tx = session.beginTransaction();
+        try {
+            T rsl = command.apply(session);
+            tx.commit();
+            return rsl;
+        } catch (final Exception e) {
+            session.getTransaction().rollback();
+            throw e;
+        } finally {
+            session.close();
+        }
     }
 
 }
